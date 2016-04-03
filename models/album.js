bbcc31a61700ed00093fc1898b4b02badeb0175a
seanvsville <seanvanho@gmail.com>
2016-04-02 17:21:17 -0700

    referenced artist to albums and embedded songs into album

--- models/album.js
+++ models/album.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose'),
-      Schema = mongoose.Schema;
+      Schema = mongoose.Schema,
+ songsSchema = new Schema({
+   name: String
+ });
 
 var AlbumSchema = new Schema({
   title: String,
-  artist: String,
+  artist: {
+    type: Schema.Types.ObjectId,
+    ref: 'Artist'
+  },
   yearReleased: String,
   albumCover: String,
-  songs: String
+  songs: [songSchema]
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
