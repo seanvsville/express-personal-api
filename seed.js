6de7509ddc8ffa48d16c6ae517030364d8fbf7aa
seanvsville <seanvanho@gmail.com>
2016-04-03 16:37:23 -0700

    more progress on seed.js file

--- seed.js
+++ seed.js
@@ -18,10 +18,10 @@ var albums_list = [
     artist: 'The Get Up Kids',
     yearReleased: 'March 14, 2002',
     albumCover: 'http://ecx.images-amazon.com/images/I/519JvdzX-WL.jpg',
-    songs: ['Overdue', 'Overdue', 'Stay Gone', 'Let the Reigns Go Loose', 'Fall From Grace', 'Grunge Pig', 'High as the Moon', 'All That I Know', 'Walking on a Wire', 'Wish You Were Here', 'Campfire Kansas', 'The Worst Idea', 'Hannah Hold On']
+    songs: ['Overdue', 'Stay Gone', 'Let the Reigns Go Loose', 'Fall From Grace', 'Grunge Pig', 'High as the Moon', 'All That I Know', 'Walking on a Wire', 'Wish You Were Here', 'Campfire Kansas', 'The Worst Idea', 'Hannah Hold On']
   },
   {
-    title: 'OK, Computer',
+    title: 'OK Computer',
     artist: 'Radiohead',
     yearReleased: 'March 21, 1997',
     albumCover: 'http://ecx.images-amazon.com/images/I/81ni71zIxIL._SL1406_.jpg',
@@ -53,23 +53,23 @@ var albums_list = [
   var artists_list = [
     {
       name: 'The Get Up Kids',
-      members: ['Matt Pryor', 'Jim Suptic', 'John Greenway', 'Rob Pope', 'James Dewees', 'Ryan Pope'],
+      members: ['Matt Pryor', 'Jim Suptic', 'John Greenway', 'Rob Pope', 'James Dewees', 'Ryan Pope']
     },
     {
       name: 'Radiohead',
-      members: ['Thom Yorke', 'Philip Selway', 'John Greenway', 'Ed OBrien', 'Colin Greenwood'],
+      members: ['Thom Yorke', 'Philip Selway', 'John Greenwood', 'Ed OBrien', 'Colin Greenwood']
     },
     {
       name: 'Kendrick Lamar',
-      members: ['Kendrick Lamar'],
+      members: ['Kendrick Lamar']
     },
     {
       name: 'Jay-Z',
-      members: ['Jay-Z'],
+      members: ['Jay-Z']
     },
     {
       name: 'The Herbaliser',
-      members: ['Ollie Trattles', 'Kaidi Tatham', 'Patrick Dawes', 'Jake Wherry', 'Chris Bowden'],
+      members: ['Ollie Trattles', 'Kaidi Tatham', 'Patrick Dawes', 'Jake Wherry', 'Chris Bowden']
     }
   ];
 
@@ -87,71 +87,41 @@ var albums_list = [
   });
 });
 
-  db.Album.remove({}, function(err, profile) {
-  console.log('remove all albums');
-  db.Album.create(albums_list, function(err, albums){
-    if (err) {
-      console.log(err);
-      return;
-  }
-  console.log(profile);
-  console.log('recreated albums');
-  console.log("created", profile.length, "albums");
-
-  });
-});
-
-db.Artist.remove({}, function(err, profile) {
-console.log('remove all profile information');
+db.Artist.remove({}, function(err, artists) {
+console.log('removed all artists');
 db.Artist.create(artists_list, function(err, artists){
   if (err) {
     console.log(err);
     return;
   }
-  console.log(artists);
-  console.log('recreated artists');
-  console.log("created", profile.length, "artists");
+  console.log('recreated all artists');
+  console.log("created", artists.length, "artists");
 
-  });
-});
 
-/*process.exit();*/
-
-    /*db.Album.remove({}, function(err, albums){
-      console.log('removed all albums');
-      albums_list.forEach(function (albumData) {
-        var album = new db.Album({
-          title: albumData.title,
-          albumCover: albumData.image,
-          releaseDate: albumData.releaseDate
-        });
-        db.Artist.findOne({name: albumData.artist}, function (err, foundArtist) {
-          console.log('found artist ' + foundArtist.name + ' for album ' + album.title);
+  db.Album.remove({}, function(err, albums){
+    console.log('removed all albums');
+    albums_list.forEach(function (albumData) {
+      var album = new db.Album({
+        title: albumData.title,
+        image: albumData.image,
+        releaseDate: albumData.releaseDate
+      });
+      db.Artist.findOne({name: albumData.artist}, function (err, foundartist) {
+        console.log('found artist ' + foundartist.name + ' for album ' + album.title);
+        if (err) {
+          console.log(err);
+          return;
+        }
+        album.artist = foundartist;
+        album.save(function(err, savedalbum){
           if (err) {
-            console.log(err);
-            return;
+            return console.log(err);
           }
-          // saves found Artist into album
-          album.artist = foundArtist;
-          album.save(function(err, savedAlbum){
-            if (err) {
-              return console.log(err);
-            }
-            console.log('saved ' + savedAlbum.title + ' by ' + foundArtist.name);
-          });
+          console.log('saved ' + savedalbum.title + ' by ' + foundartist.name);
         });
       });
-    });*/
+    });
+  });
 
-
-
-// var new_campsite = {description: 'Sharp rocks. Middle of nowhere.'}
-
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log('Error:', err);
-//   }
-
-//   console.log('Created new campsite', campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+});
+});
