6bf08927f7145ecca9d1702df091be657fc54cf2
seanvsville <seanvanho@gmail.com>
2016-04-04 09:18:32 -0700

    heroku ready

--- models/index.js
+++ models/index.js
@@ -4,5 +4,5 @@ mongoose.connect( process.env.MONGOLAB_URI ||
                   'mongodb://localhost/personal-api');
 
 module.exports.Profile = require('./profile.js');
-module.exports.Album = require('./album.js');
-module.exports.Artist = require('./artist.js');
+  module.exports.Album = require('./album.js');
+ module.exports.Artist = require('./artist.js');
